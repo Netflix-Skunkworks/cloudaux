c148cd91794f916c3aeac2a6cbd6ac4c7f9cae0a
Patrick Kelley <pkelley@netflix.com>
2016-11-16 14:39:40 -0800

    Fixing module name in docstring

--- cloudaux/aws/sqs.py
+++ cloudaux/aws/sqs.py
@@ -1,5 +1,5 @@
 """
-.. module: bobo.aws.sqs
+.. module: cloudaux.aws.sqs
     :platform: Unix
     :copyright: (c) 2015 by Netflix Inc., see AUTHORS for more
     :license: Apache, see LICENSE for more details.
