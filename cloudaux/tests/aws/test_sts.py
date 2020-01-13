d146e7bdf35720c346e16c96bab484b369ca84ca
Mike Grima <mgrima@netflix.com>
2020-01-13 11:27:16 -0800

    Added tests for boto3 retry configuration

--- cloudaux/tests/aws/test_sts.py
+++ cloudaux/tests/aws/test_sts.py
@@ -42,3 +42,44 @@ def test_boto3_cached_conn_default():
 
         # Assert
         assert 'PolicyArns' not in conn.assume_role.call_args.kwargs
+
+
+def test_boto3_cached_conn_retry_config(sts):
+    from cloudaux.aws.sts import _client
+    import cloudaux.aws.sts
+
+    def mock_client(*args, **kwargs):
+        with patch('boto3.session.Session') as p:
+            _client(*args, **kwargs)
+
+        return p
+
+    # With the default:
+    with patch('cloudaux.aws.sts._client', mock_client):
+        conn = boto3_cached_conn('s3')
+        assert conn.mock_calls[1].kwargs['config'].retries == {'max_attempts': 10}
+        cloudaux.aws.sts.CACHE = {}
+
+    # With STS role assumption:
+    conn_details = {
+        'account_number': '111111111111',
+        'assume_role': 'role_one',
+        'region': 'us-east-1'
+    }
+    with patch('cloudaux.aws.sts._client', mock_client):
+        conn = boto3_cached_conn('s3', **conn_details)
+        assert conn.mock_calls[1].kwargs['config'].retries == {'max_attempts': 10}
+        cloudaux.aws.sts.CACHE = {}
+
+    # With a specified retry Config:
+    with patch('cloudaux.aws.sts._client', mock_client):
+        conn = boto3_cached_conn('s3', retry_max_attempts=1000)
+        assert conn.mock_calls[1].kwargs['config'].retries == {'max_attempts': 1000}
+        cloudaux.aws.sts.CACHE = {}
+
+    # With STS role assumption:
+    conn_details['retry_max_attempts'] = 1000
+    with patch('cloudaux.aws.sts._client', mock_client):
+        conn = boto3_cached_conn('s3', **conn_details)
+        assert conn.mock_calls[1].kwargs['config'].retries == {'max_attempts': 1000}
+        cloudaux.aws.sts.CACHE = {}
