ee1033daebc20d5c89743832ee372a932c04a371
Patrick Sanders <psanders@netflix.com>
2020-07-23 10:26:56 -0700

    bump version

--- cloudaux/__about__.py
+++ cloudaux/__about__.py
@@ -13,10 +13,10 @@ __title__ = 'cloudaux'
 __summary__ = 'Cloud Auxiliary is a python wrapper and orchestration module for interacting with cloud providers'
 __uri__ = 'https://github.com/Netflix-Skunkworks/cloudaux'
 
-__version__ = '1.8.1'
+__version__ = '1.8.2'
 
 __author__ = 'The Cloudaux Developers'
 __email__ = 'oss@netflix.com'
 
 __license__ = 'Apache License, Version 2.0'
-__copyright__ = 'Copyright 2019 %s' % __author__
+__copyright__ = 'Copyright 2020 %s' % __author__
