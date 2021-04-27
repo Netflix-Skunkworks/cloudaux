5c9572fde63ed0ad2168fb23f9185e51da720d69
Patrick Sanders <psanders@netflix.com>
2021-04-27 14:56:24 -0700

    Fix AWS pagination for responses with empty results

--- cloudaux/aws/decorators.py
+++ cloudaux/aws/decorators.py
@@ -67,10 +67,10 @@ def paginated(response_key, request_pagination_marker="Marker", response_paginat
                 response = func(*args, **kwargs)
                 results.extend(response[response_key])
 
-                # If the response contained results, and the "next" pagination marker is in the response, then paginate.
-                if response[response_key] and response.get(response_pagination_marker):
+                # If the "next" pagination marker is in the response, then paginate. Responses may not always have
+                # items in the response_key, so we should only key off of the response_pagination_marker.
+                if response.get(response_pagination_marker):
                     kwargs.update({request_pagination_marker: response[response_pagination_marker]})
-
                 else:
                     break
             return results
