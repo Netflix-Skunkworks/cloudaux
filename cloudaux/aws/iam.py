8e06fc45b761d107768b10823795fa8f076235e9
ConsoleMe <ccastrapel@netflix.com>
2021-07-27 22:18:09 -0700

    Adds support for get_user_managed_policy_documents

--- cloudaux/aws/iam.py
+++ cloudaux/aws/iam.py
@@ -200,6 +200,19 @@ def get_all_managed_policies(client=None, **kwargs):
 
 @sts_conn('iam', service_type='client')
 @rate_limited()
+def get_user_managed_policy_documents(user, client=None, **kwargs):
+    """Retrieve the currently active policy version document for every managed policy that is attached to the user."""
+    policies = get_user_managed_policies(user, force_client=client)
+
+    policy_names = (policy['name'] for policy in policies)
+    delayed_gmpd_calls = (delayed(get_managed_policy_document)(policy['arn'], force_client=client) for policy
+                          in policies)
+    policy_documents = Parallel(n_jobs=20, backend="threading")(delayed_gmpd_calls)
+
+    return dict(zip(policy_names, policy_documents))
+
+@sts_conn('iam', service_type='client')
+@rate_limited()
 def get_role_managed_policy_documents(role, client=None, **kwargs):
     """Retrieve the currently active policy version document for every managed policy that is attached to the role."""
     policies = get_role_managed_policies(role, force_client=client)
